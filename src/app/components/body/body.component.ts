e9573473ab8a6e0a08dac025dd76e3836ceb5bf8
Kenjhy <kenjhyx@hotmail.com>
2020-09-08 15:03:02 -0500

    hola mundo con angular, menu y otras cosas

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-body',
+    templateUrl: './body.component.html'
+})
+
+export class BodyComponent {
+
+    mostrar=true; //Boton mostrar o ocultar
+
+    frase: any={
+        mensaje: 'Esto es autor',
+        autor: 'Esto es mensaje'
+    };
+
+    //En base a arreglos trabaja
+    personajes: string[] = ['daniel','venon', 'otroenemigo'];
+
+}
